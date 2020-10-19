1c5d4eba36eaa61ff0a8b0fca75cdfeedcb9e8d4
Nathan Dougan <ndougan23@outlook.com>
2020-10-19 18:44:13 -0400

    Implemented DataReader

--- FlightFinder/Pages/Flights.cshtml.cs
+++ FlightFinder/Pages/Flights.cshtml.cs
@@ -11,35 +11,51 @@ using MySql.Data.MySqlClient;
 using FlightFinder.Data;
 using Microsoft.EntityFrameworkCore;
 using FlightFinder.Model;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FlightFinder.Pages
 {
     public class FlightsModel : PageModel
     {
-        private readonly ApplicationDbContext _db;
+        public string YourText;
 
-        public FlightsModel(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-
-        public IEnumerable<Flight> Flights { get; set; }
-      
-        public void OnGet() 
-        {
-            //Flights = await _db.Flight.ToListAsync();
+         public void OnGet() {
             TableFill();
+            YourText = "test";
         }
+        
+        void TableFill() {
+            const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
 
-        void TableFill()
-        {
-            using var connection = new MySqlConnection("server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;");
-            connection.Open();
-            MySqlDataAdapter sqlDa = new MySqlDataAdapter("FlightViewAll", connection);
-            sqlDa.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
-            DataTable dtbl = new DataTable();
-            sqlDa.Fill(dtbl);
-            
+            try {
+                conn.Open(); 
+
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM flight", conn);
+                cmd.CommandType = CommandType.Text;
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read()) { // remove console.writeline and set each equal to variable to pass them to the html
+                    Console.WriteLine(string.Format("flight_id = {0}", rdr["flight_id"].ToString()));
+                    Console.WriteLine(string.Format("departure_city = {0}", rdr["departure_city"].ToString()));
+                    Console.WriteLine(string.Format("arrival_city = {0}", rdr["arrival_city"].ToString()));
+                    Console.WriteLine(string.Format("flight_date = {0}", rdr["flight_date"].ToString()));
+                    Console.WriteLine(string.Format("departure_time = {0}", rdr["departure_time"].ToString()));
+                    Console.WriteLine(string.Format("arrival_time = {0}", rdr["arrival_time"].ToString()));
+                    Console.WriteLine(string.Format("estimated_flight_time = {0}", rdr["estimated_flight_time"].ToString()));
+                    Console.WriteLine(string.Format("airline = {0}", rdr["airline"].ToString()));
+                    Console.WriteLine(string.Format("aircraft_registration = {0}", rdr["aircraft_registration"].ToString()));
+                    Console.WriteLine(string.Format("aircraft_type = {0}", rdr["aircraft_type"].ToString()));
+                    Console.WriteLine(string.Format("flight_distance = {0}", rdr["flight_distance"].ToString()));
+                    Console.WriteLine(string.Format("total_seats = {0}", rdr["total_seats"].ToString()));
+                    Console.WriteLine(string.Format("open_seats = {0}", rdr["open_seats"].ToString()));
+                }
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine("{oops - {0}", ex.Message);
+            }
+            conn.Dispose(); 
         }
     }  
 }
