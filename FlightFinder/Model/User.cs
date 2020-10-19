3732d306accd16a540a5e7050eb788654c4f8abf
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2020-10-18 22:51:44 -0400

    Added registration
    added a photo of a user icon for accessing account creation. added the model for user table

---
+++ FlightFinder/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightFinder.Model
+{
+    public class User
+    {
+        [Key]
+        public int User_ID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public DateTime Account_Creation_Date { get; set; }
+
+
+    }
+}
--- FlightFinder/Pages/Flights.cshtml.cs
+++ FlightFinder/Pages/Flights.cshtml.cs
@@ -8,14 +8,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
-
+using FlightFinder.Data;
+using Microsoft.EntityFrameworkCore;
+using FlightFinder.Model;
 
 namespace FlightFinder.Pages
 {
     public class FlightsModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public FlightsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Flight> Flights { get; set; }
+      
         public void OnGet() 
         {
+            //Flights = await _db.Flight.ToListAsync();
             TableFill();
         }
 
