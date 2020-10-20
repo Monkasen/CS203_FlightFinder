e1ed2e9aa7d2af9f933e144395fcc5d6b7d5abb3
Nathan Dougan <ndougan23@outlook.com>
2020-10-20 15:50:18 -0400

    Fixed overwriting of data.

--- FlightFinder/Pages/Flights.cshtml.cs
+++ FlightFinder/Pages/Flights.cshtml.cs
@@ -17,21 +17,34 @@ namespace FlightFinder.Pages
 {
     public class FlightsModel : PageModel
     {
-        public string Flight_ID;
-        public string Departure_City;
-        public string Arrival_City;
-        public string Departure_Time;
-        public string Arrival_Time;
-        public string Flight_Date;
-        public string Airline_Name;
-        public string Aircraft_Reg_Num;
-        public string Aircraft_Type;
-        public string Flight_Distance;
-        public string E_Flight_Time;
-        public string Total_Seats;
-        public string Open_Seats;
-        public string YourText;
-        public int TableSize = 0;
+        List<string> F_ID = new List<string>();
+        public string[] Flight_ID;
+        List<string> D_City = new List<string>();
+        public string[] Departure_City;
+        List<string> A_City = new List<string>();
+        public string[] Arrival_City;
+        List<string> D_Time = new List<string>();
+        public string[] Departure_Time;
+        List<string> A_Time = new List<string>();
+        public string[] Arrival_Time;
+        List<string> F_Date = new List<string>();
+        public string[] Flight_Date;
+        List<string> A_Name = new List<string>();
+        public string[] Airline_Name;
+        List<string> A_Registration = new List<string>();
+        public string[] Aircraft_Reg_Num;
+        List<string> A_Type = new List<string>();
+        public string[] Aircraft_Type;
+        List<string> F_Distance = new List<string>();
+        public string[] Flight_Distance;
+        List<string> E_F_Time = new List<string>();
+        public string[] E_Flight_Time;
+        List<string> T_Seats = new List<string>();
+        public string[] Total_Seats;
+        List<string> O_Seats = new List<string>();
+        public string[] Open_Seats;
+
+        public int TableSize = 1;
 
          public void OnGet() {
             TableFill();
@@ -49,33 +62,35 @@ namespace FlightFinder.Pages
                 cmd.CommandType = CommandType.Text;
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 
-                while (rdr.Read()) { 
-                    string F_ID = (string.Format("{0}", rdr["flight_id"].ToString()));
-                    Flight_ID = F_ID;                    
-                    string D_City = (string.Format("{0}", rdr["departure_city"].ToString()));
-                    Departure_City = D_City;
-                    string A_City = (string.Format("{0}", rdr["arrival_city"].ToString()));
-                    Arrival_City = A_City;
-                    string F_Date = (string.Format("{0}", rdr["flight_date"].ToString()));
-                    Flight_Date = F_Date;
-                    string D_Time = (string.Format("{0}", rdr["departure_time"].ToString()));
-                    Departure_Time = D_Time;
-                    string A_Time = (string.Format("{0}", rdr["arrival_time"].ToString()));
-                    Arrival_Time = A_Time;
-                    string E_F_Time = (string.Format("{0}", rdr["estimated_flight_time"].ToString()));
-                    E_Flight_Time = E_F_Time;                  
-                    string Airline = (string.Format("{0}", rdr["airline"].ToString()));
-                    Airline_Name = Airline;
-                    string A_Registration = (string.Format("{0}", rdr["aircraft_registration"].ToString()));
-                    Aircraft_Reg_Num = A_Registration;
-                    string A_Type = (string.Format("{0}", rdr["aircraft_type"].ToString()));
-                    Aircraft_Type = A_Type;
-                    string F_Distance = (string.Format("{0} miles", rdr["flight_distance"].ToString()));
-                    Flight_Distance = F_Distance;
-                    string T_Seats = (string.Format("{0}", rdr["total_seats"].ToString()));
-                    Total_Seats = T_Seats;
-                    string O_Seats = (string.Format("{0}", rdr["open_seats"].ToString()));
-                    Open_Seats = O_Seats;
+                while (rdr.Read()) {
+                    
+                    F_ID.Add(string.Format("{0}", rdr["flight_id"].ToString()));
+                    Flight_ID = F_ID.ToArray();
+
+                    D_City.Add(string.Format("{0}", rdr["departure_city"].ToString()));
+                    Departure_City = D_City.ToArray();
+                    A_City.Add(string.Format("{0}", rdr["arrival_city"].ToString()));
+                    Arrival_City = A_City.ToArray();
+                    F_Date.Add(string.Format("{0}", rdr["flight_date"].ToString()));
+                    Flight_Date = F_Date.ToArray();
+                    D_Time.Add(string.Format("{0}", rdr["departure_time"].ToString()));
+                    Departure_Time = D_Time.ToArray();
+                    A_Time.Add(string.Format("{0}", rdr["arrival_time"].ToString()));
+                    Arrival_Time = A_Time.ToArray(); 
+                    E_F_Time.Add(string.Format("{0}", rdr["estimated_flight_time"].ToString()));
+                    E_Flight_Time = E_F_Time.ToArray();
+                    A_Name.Add(string.Format("{0}", rdr["airline"].ToString()));
+                    Airline_Name = A_Name.ToArray();
+                    A_Registration.Add(string.Format("{0}", rdr["aircraft_registration"].ToString()));
+                    Aircraft_Reg_Num = A_Registration.ToArray();
+                    A_Type.Add(string.Format("{0}", rdr["aircraft_type"].ToString()));
+                    Aircraft_Type = A_Type.ToArray();
+                    F_Distance.Add(string.Format("{0} miles", rdr["flight_distance"].ToString()));
+                    Flight_Distance = F_Distance.ToArray();
+                    T_Seats.Add(string.Format("{0}", rdr["total_seats"].ToString()));
+                    Total_Seats = T_Seats.ToArray();
+                    O_Seats.Add(string.Format("{0}", rdr["open_seats"].ToString()));
+                    Open_Seats = O_Seats.ToArray();
 
                     TableSize++;
                 }
