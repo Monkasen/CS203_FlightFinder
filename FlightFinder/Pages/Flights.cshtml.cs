8b40d7b3910532616661a0864d69d0b342d9818b
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2020-10-28 22:40:21 -0400

    Added Filter text boxes and setup

--- FlightFinder/Pages/Flights.cshtml.cs
+++ FlightFinder/Pages/Flights.cshtml.cs
@@ -29,7 +29,7 @@ namespace FlightFinder.Pages
         public string[] Arrival_Time;
         List<string> F_Date = new List<string>();
         public string[] Flight_Date;
-        List<string> A_Name = new List<string>();
+        List<string> Air_Name = new List<string>();
         public string[] Airline_Name;
         List<string> A_Registration = new List<string>();
         public string[] Aircraft_Reg_Num;
@@ -46,10 +46,113 @@ namespace FlightFinder.Pages
 
         public int TableSize = 0;
 
-         public void OnGet() {
+        public string From_TextBox { get; set; }
+
+        public string To_TextBox { get; set; }
+
+        public DateTime? DepartDate_TextBox { get; set; }
+
+        public string Airline_TextBox { get; set; }
+
+        public string Depart_Time_TextBox { get; set; }
+
+        public string Arrival_Time_TextBox { get; set; }
+
+        public void OnGet() {
             TableFill();
         }
+
+        public bool FilterResults()
+        {
+            TableSize = 0;
+            D_City.Clear();
+            A_City.Clear();
+            D_Time.Clear();
+            A_Time.Clear();
+            F_Date.Clear();
+            Air_Name.Clear();
+            const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable dataSet = new DataTable();
+
+
+            if (!String.IsNullOrEmpty(Request.Form["From"]))
+            {
+                From_TextBox = Request.Form["From"];
+            }
+
+            if (!String.IsNullOrEmpty(Request.Form["To"]))
+            {
+                To_TextBox = Request.Form["To"];
+            }
+            if (!String.IsNullOrEmpty(Request.Form["DepartDate"]))
+            {
+                DepartDate_TextBox = DateTime.Parse(Request.Form["DepartDate"]);
+            }
+            if (!String.IsNullOrEmpty(Request.Form["Airline"]))
+            {
+                Airline_TextBox = Request.Form["Airline"];
+            }
+            
+
+            string cmdtxt = $"SELECT * FROM flight WHERE Departure_Airport = '{this.From_TextBox}' OR Departure_City = '{this.From_TextBox}' OR Arrival_City = '{this.To_TextBox}' OR Arrival_Airport = '{this.To_TextBox}';";
+            MySqlCommand cmd = new MySqlCommand(cmdtxt, conn);
+            cmd.CommandType = CommandType.Text;
+            adapter.SelectCommand = cmd;
+            adapter.Fill(dataSet);
+
+            if (dataSet.Rows.Count < 1)
+            {
+                conn.Dispose();
+                
+                return false;
+            }
+            else
+            {
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+
+                    F_ID.Add(string.Format("{0}", rdr["flight_id"].ToString()));
+                    Flight_ID = F_ID.ToArray();
+                    D_City.Add(string.Format("{0}", rdr["departure_city"].ToString()));
+                    Departure_City = D_City.ToArray();
+                    A_City.Add(string.Format("{0}", rdr["arrival_city"].ToString()));
+                    Arrival_City = A_City.ToArray();
+                    F_Date.Add(string.Format("{0}", rdr["flight_date"].ToString()));
+                    Flight_Date = F_Date.ToArray();
+                    D_Time.Add(string.Format("{0}", rdr["departure_time"].ToString()));
+                    Departure_Time = D_Time.ToArray();
+                    A_Time.Add(string.Format("{0}", rdr["arrival_time"].ToString()));
+                    Arrival_Time = A_Time.ToArray();
+                    E_F_Time.Add(string.Format("{0}", rdr["estimated_flight_time"].ToString()));
+                    E_Flight_Time = E_F_Time.ToArray();
+                    Air_Name.Add(string.Format("{0}", rdr["airline"].ToString()));
+                    Airline_Name = Air_Name.ToArray();
+                    A_Registration.Add(string.Format("{0}", rdr["aircraft_registration"].ToString()));
+                    Aircraft_Reg_Num = A_Registration.ToArray();
+                    A_Type.Add(string.Format("{0}", rdr["aircraft_type"].ToString()));
+                    Aircraft_Type = A_Type.ToArray();
+                    F_Distance.Add(string.Format("{0} miles", rdr["flight_distance"].ToString()));
+                    Flight_Distance = F_Distance.ToArray();
+                    T_Seats.Add(string.Format("{0}", rdr["total_seats"].ToString()));
+                    Total_Seats = T_Seats.ToArray();
+                    O_Seats.Add(string.Format("{0}", rdr["open_seats"].ToString()));
+                    Open_Seats = O_Seats.ToArray();
+
+                    TableSize++;
+                }
+                conn.Dispose();
+                return true;
+            }
+        }
+
         
+
+        
+
         public void TableFill() {
             const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
             MySqlConnection conn = new MySqlConnection(connectionString);
@@ -77,8 +180,8 @@ namespace FlightFinder.Pages
                     Arrival_Time = A_Time.ToArray(); 
                     E_F_Time.Add(string.Format("{0}", rdr["estimated_flight_time"].ToString()));
                     E_Flight_Time = E_F_Time.ToArray();
-                    A_Name.Add(string.Format("{0}", rdr["airline"].ToString()));
-                    Airline_Name = A_Name.ToArray();
+                    Air_Name.Add(string.Format("{0}", rdr["airline"].ToString()));
+                    Airline_Name = Air_Name.ToArray();
                     A_Registration.Add(string.Format("{0}", rdr["aircraft_registration"].ToString()));
                     Aircraft_Reg_Num = A_Registration.ToArray();
                     A_Type.Add(string.Format("{0}", rdr["aircraft_type"].ToString()));
@@ -130,9 +233,14 @@ namespace FlightFinder.Pages
                 Console.WriteLine("DEBUG - Book");
                 return Redirect($"/Book?Flight_ID={parsedID}");
             }
-            else if (submit[0] == 'S') { // ...or to be saved.
+            else if (submit[0] == 'S') { // ...saved...
                 Console.WriteLine("DEBUG - Save");
                 SaveFlightToDB(parsedID);
+            }
+            else if(submit[0] == 'F') {//... or filtered through
+                Console.WriteLine("Filter results");
+                FilterResults();
+                return RedirectToPage();
             }
             else {
                 Console.WriteLine("DEBUG - Error");
@@ -141,5 +249,21 @@ namespace FlightFinder.Pages
             TableFill();
             return RedirectToPage();
         }
+
+
+        public async Task<IActionResult> OnPostFilter()
+        {
+            if(FilterResults())
+            {
+                return RedirectToPage();
+            }
+            else
+            {
+                Console.WriteLine("No Flights match your search");
+                return RedirectToPage();
+            }
+        }
     }     
+
+
 }
