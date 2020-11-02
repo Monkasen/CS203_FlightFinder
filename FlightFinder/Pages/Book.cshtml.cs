6ba88aa64087351dd5e8435eb03e38b598cd9d13
Nathan Dougan <ndougan23@outlook.com>
2020-11-01 21:40:06 -0500

    Started work on flight booking

--- FlightFinder/Pages/Book.cshtml.cs
+++ FlightFinder/Pages/Book.cshtml.cs
@@ -1,18 +1,179 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using FlightFinder.Data;
+using Microsoft.EntityFrameworkCore;
+using FlightFinder.Model;
+using System.Security.Cryptography.X509Certificates;
 
-namespace FlightFinder.Pages
-{
+namespace FlightFinder.Pages {
     public class BookModel : PageModel
     {
-        public string Flight_ID;
+        #region flight_table
+        List<string> F_ID = new List<string>();
+        public string[] Flight_ID;
+        List<string> D_City = new List<string>();
+        public string[] Departure_City;
+        List<string> A_City = new List<string>();
+        public string[] Arrival_City;
+        List<string> D_Time = new List<string>();
+        public string[] Departure_Time;
+        List<string> A_Time = new List<string>();
+        public string[] Arrival_Time;
+        List<string> F_Date = new List<string>();
+        public string[] Flight_Date;
+        List<string> A_Name = new List<string>();
+        public string[] Airline_Name;
+        List<string> A_Registration = new List<string>();
+        public string[] Aircraft_Reg_Num;
+        List<string> A_Type = new List<string>();
+        public string[] Aircraft_Type;
+        List<string> F_Distance = new List<string>();
+        public string[] Flight_Distance;
+        List<string> E_F_Time = new List<string>();
+        public string[] E_Flight_Time;
+        List<string> T_Seats = new List<string>();
+        public string[] Total_Seats;
+        List<string> O_Seats = new List<string>();
+        public string[] Open_Seats;
+        #endregion
+
+        #region card_list
+        public int ListSize = 0;
+        List<string> C_Num = new List<string>();
+        public string[] Card_Number;
+        #endregion
+
+        public string User_ID;
+        public string Seats_Reserved;
+        public string LastFour_Card;
+
+        int tempNum = 0;
+
         public void OnGet() {
             Flight_ID = Request.Query["Flight_ID"];
-            Console.WriteLine($"You are reserving a flight with internal ID: {Flight_ID}");
+            tempNum = Int32.Parse(Flight_ID[0]);
+            User_ID = "1"; // TEMPORARY VALUE TO STORE USER ID, DEFAULTS TO NULL USER, LATER CHANGE TO WHATEVER USER IS BOOKING THE FLIGHT!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine($"You are reserving a flight with internal ID: {Flight_ID[0]}");
+            FlightTableFill();
+            CardListFill();
+        }
+
+        public void FlightTableFill() {
+            const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            try {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand($"SELECT * FROM flight WHERE flight.flight_id = {Flight_ID[0]}", conn);
+                cmd.CommandType = CommandType.Text;
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read()) {
+
+                    F_ID.Add(string.Format("{0}", rdr["flight_id"].ToString()));
+                    this.Flight_ID = F_ID.ToArray();
+                    D_City.Add(string.Format("{0}", rdr["departure_city"].ToString()));
+                    this.Departure_City = D_City.ToArray();
+                    A_City.Add(string.Format("{0}", rdr["arrival_city"].ToString()));
+                    this.Arrival_City = A_City.ToArray();
+                    F_Date.Add(string.Format("{0}", rdr["flight_date"].ToString()));
+                    this.Flight_Date = F_Date.ToArray();
+                    D_Time.Add(string.Format("{0}", rdr["departure_time"].ToString()));
+                    this.Departure_Time = D_Time.ToArray();
+                    A_Time.Add(string.Format("{0}", rdr["arrival_time"].ToString()));
+                    this.Arrival_Time = A_Time.ToArray();
+                    E_F_Time.Add(string.Format("{0}", rdr["estimated_flight_time"].ToString()));
+                    this.E_Flight_Time = E_F_Time.ToArray();
+                    A_Registration.Add(string.Format("{0}", rdr["aircraft_registration"].ToString()));
+                    this.Aircraft_Reg_Num = A_Registration.ToArray();
+                    A_Type.Add(string.Format("{0}", rdr["aircraft_type"].ToString()));
+                    this.Aircraft_Type = A_Type.ToArray();
+                    F_Distance.Add(string.Format("{0} miles", rdr["flight_distance"].ToString()));
+                    this.Flight_Distance = F_Distance.ToArray();
+                    T_Seats.Add(string.Format("{0}", rdr["total_seats"].ToString()));
+                    this.Total_Seats = T_Seats.ToArray();
+                    O_Seats.Add(string.Format("{0}", rdr["open_seats"].ToString()));
+                    this.Open_Seats = O_Seats.ToArray();
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("{oops - {0}", ex.Message);
+            }
+            conn.Dispose();
+        }
+
+        public void CardListFill() {
+            const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            try {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand($"SELECT * FROM payment_card WHERE payment_card.user_id = {User_ID}", conn);
+                cmd.CommandType = CommandType.Text;
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read()) {
+
+                    C_Num.Add(string.Format("{0}", rdr["card_number"].ToString()));
+                    this.Card_Number = C_Num.ToArray();
+                    Card_Number[ListSize] = Card_Number[ListSize].Substring(Card_Number[ListSize].Length - 4); // Get last 4 digits of the card number
+
+                    ++ListSize;
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("{oops - {0}", ex.Message);
+            }
+            conn.Dispose();
+        }
+
+        public void BookFlight() {
+            const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            string txtcmd = $"INSERT INTO saved_flights (User_ID, Flight_ID, Seats_Reserved) " +
+                $"VALUES (@User_ID, @Flight_ID )";
+            MySqlCommand cmd = new MySqlCommand(txtcmd, conn);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.AddWithValue("@User_ID", 1); // THIS IS WHERE WE ASSIGN WHAT USER BOOKS THE FLIGHT, RIGHT NOW IT DEFAULTS TO USER 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            cmd.Parameters.AddWithValue("@Flight_ID", Flight_ID);
+            cmd.Parameters.AddWithValue("@Seats_Reserved", Seats_Reserved);
+            cmd.Parameters.AddWithValue("@LastFour_Card", LastFour_Card);
+            cmd.Prepare();
+            cmd.ExecuteReader();
+
+            conn.Dispose();
+        }
+
+        public async Task<IActionResult> OnPost(string submit) { 
+            if (submit[0] == 'C') { 
+                Console.WriteLine("DEBUG - Confirm Booking");
+                //BookFlight();
+                return Redirect($"/Flights");
+            }
+            else if (submit[0] == 'N') {
+                Console.WriteLine("DEBUG - New Card");
+                Console.WriteLine(tempNum);
+                return Page();
+                //return Redirect($"/Payment?Flight_ID={tempNum}");
+            }
+            else {
+                Console.WriteLine("DEBUG - Error");
+                // error text
+                return RedirectToPage();
+            }
         }
     }
 }
--- FlightFinder/Pages/User.cshtml.cs
+++ FlightFinder/Pages/User.cshtml.cs
@@ -18,6 +18,9 @@ namespace FlightFinder.Pages
     public class UserModel : PageModel
     {
         public string User_ID;
+
+        #region saved_flights variables
+        public int TableSize = 0;
         List<string> F_ID = new List<string>();
         public string[] Flight_ID;
         List<string> D_City = new List<string>();
@@ -44,20 +47,48 @@ namespace FlightFinder.Pages
         public string[] Total_Seats;
         List<string> O_Seats = new List<string>();
         public string[] Open_Seats;
-
-        public int TableSize = 0;
+        #endregion
+        #region booked_flights variables
+        public int B_TableSize = 0;
+        List<string> B_F_ID = new List<string>();
+        public string[] B_Flight_ID;
+        List<string> B_D_City = new List<string>();
+        public string[] B_Departure_City;
+        List<string> B_A_City = new List<string>();
+        public string[] B_Arrival_City;
+        List<string> B_D_Time = new List<string>();
+        public string[] B_Departure_Time;
+        List<string> B_A_Time = new List<string>();
+        public string[] B_Arrival_Time;
+        List<string> B_F_Date = new List<string>();
+        public string[] B_Flight_Date;
+        List<string> B_A_Name = new List<string>();
+        public string[] B_Airline_Name;
+        List<string> B_A_Registration = new List<string>();
+        public string[] B_Aircraft_Reg_Num;
+        List<string> B_A_Type = new List<string>();
+        public string[] B_Aircraft_Type;
+        List<string> B_F_Distance = new List<string>();
+        public string[] B_Flight_Distance;
+        List<string> B_E_F_Time = new List<string>();
+        public string[] B_E_Flight_Time;
+        List<string> B_T_Seats = new List<string>();
+        public string[] B_Total_Seats;
+        List<string> B_O_Seats = new List<string>();
+        public string[] B_Open_Seats;
+        #endregion      
 
         public void OnGet() {
             User_ID = Request.Query["User_ID"];
             if (User_ID == null) { // If nothing is retrieved, default to user 1. Later set this to error page, or redirect to login page.
                 User_ID = "1";
             }
-            
-            TableFill();
-            Console.WriteLine($"THIS IS ON USER PAGE: {User_ID}");
+
+            SavedTableFill();
+            BookedTableFill();
         }
 
-        public void TableFill()
+        public void SavedTableFill()
         {
             const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
             MySqlConnection conn = new MySqlConnection(connectionString);
@@ -99,6 +130,55 @@ namespace FlightFinder.Pages
                     Open_Seats = O_Seats.ToArray();
 
                     TableSize++;
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("{oops - {0}", ex.Message);
+            }
+            conn.Dispose();
+        }
+
+        public void BookedTableFill() {
+            const string connectionString = "server=73.249.227.33;user id=admin;password=flightfinder20;database=FlightFinder;port=3306;persistsecurityinfo=True;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            try {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand($"SELECT * FROM flight, booked_flights WHERE user_id = {User_ID} AND booked_flights.flight_id = flight.flight_id", conn);
+                cmd.CommandType = CommandType.Text;
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read()) {
+
+                    B_F_ID.Add(string.Format("{0}", rdr["flight_id"].ToString()));
+                    B_Flight_ID = B_F_ID.ToArray();
+                    B_D_City.Add(string.Format("{0}", rdr["departure_city"].ToString()));
+                    B_Departure_City = B_D_City.ToArray();
+                    B_A_City.Add(string.Format("{0}", rdr["arrival_city"].ToString()));
+                    B_Arrival_City = B_A_City.ToArray();
+                    B_F_Date.Add(string.Format("{0}", rdr["flight_date"].ToString()));
+                    B_Flight_Date = B_F_Date.ToArray();
+                    B_D_Time.Add(string.Format("{0}", rdr["departure_time"].ToString()));
+                    B_Departure_Time = B_D_Time.ToArray();
+                    B_A_Time.Add(string.Format("{0}", rdr["arrival_time"].ToString()));
+                    B_Arrival_Time = B_A_Time.ToArray();
+                    B_E_F_Time.Add(string.Format("{0}", rdr["estimated_flight_time"].ToString()));
+                    B_E_Flight_Time = B_E_F_Time.ToArray();
+                    B_A_Name.Add(string.Format("{0}", rdr["airline"].ToString()));
+                    B_Airline_Name = B_A_Name.ToArray();
+                    B_A_Registration.Add(string.Format("{0}", rdr["aircraft_registration"].ToString()));
+                    B_Aircraft_Reg_Num = B_A_Registration.ToArray();
+                    B_A_Type.Add(string.Format("{0}", rdr["aircraft_type"].ToString()));
+                    B_Aircraft_Type = B_A_Type.ToArray();
+                    B_F_Distance.Add(string.Format("{0} miles", rdr["flight_distance"].ToString()));
+                    B_Flight_Distance = B_F_Distance.ToArray();
+                    B_T_Seats.Add(string.Format("{0}", rdr["total_seats"].ToString()));
+                    B_Total_Seats = B_T_Seats.ToArray();
+                    B_O_Seats.Add(string.Format("{0}", rdr["open_seats"].ToString()));
+                    B_Open_Seats = B_O_Seats.ToArray();
+
+                    B_TableSize++;
                 }
             }
             catch (Exception ex) {
